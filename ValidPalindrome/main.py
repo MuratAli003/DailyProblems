92ab10b73943dfa4142ff0b7a8fe007a6b56c166
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2024-10-27 14:13:31 +0300

    ValidPalindrome added

---
+++ ValidPalindrome/main.py
@@ -0,0 +1,25 @@
+
+"""
+    Tüm büyük harfleri küçük harflere dönüştürdükten ve alfasayısal olmayan tüm karakterleri kaldırdıktan sonra aynı şeyi ileri ve geri okursa, bir ifade bir palindromdur. Alfasayısal karakterler harfleri ve sayıları içerir.
+
+    Bir dize s verildiğinde, palindrom ise true, aksi takdirde false döndürün.
+
+    Example 1:
+
+    Input: s = "A man, a plan, a canal: Panama"
+    Output: true
+    Explanation: "amanaplanacanalpanama" is a palindrome.
+
+"""
+
+def isPalindrome(s):
+        
+        s = s.lower().replace(" ","")
+
+        char = "~`!@#$%^&*()_-+=[{]}\|;:'\",<.>/?"
+
+        for i in char:
+
+            s = s.replace(i,"")
+        
+        return s == s[::-1]
