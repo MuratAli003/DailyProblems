1d371d428f3953bc0338a1efef6f3738fc8c297c
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2024-10-27 13:54:43 +0300

    ClearDigits added

---
+++ ClearDigits/main.py
@@ -0,0 +1,32 @@
+"""
+Size bir dize s verilir.
+
+Göreviniz, bu işlemi tekrar tekrar yaparak tüm rakamları kaldırmaktır:
+
+İlk rakamı ve solundaki en yakın rakam olmayan karakteri silin.
+
+Tüm rakamları kaldırdıktan sonra ortaya çıkan dizeyi döndürün.
+
+Örnek 1:
+
+Input: s = "abc34"
+
+Output: "a"
+
+"""
+
+
+def clearDigits(self, s):
+        
+        result = []
+
+        for i in s:
+
+            if i.isdigit() and result:
+
+                result.pop()
+
+            else:
+                result.append(i)
+        
+        return "".join(result)
