41ba4fe8b42b0b796734d6db66d450bc6aeee6c7
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2024-11-05 14:35:02 +0300

    SymmetricTree added

--- SameTree/main.cs
+++ SameTree/main.cs
@@ -1,3 +1,15 @@
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
         
---
+++ SymmetricTree/main.cs
@@ -0,0 +1,30 @@
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class Solution {
+
+    bool IsSame(TreeNode p,TreeNode q){
+
+        if(p == null || q == null){
+
+            return p == q;
+        }
+
+        return (p.val == q.val) && IsSame(p.left,q.right) && IsSame(p.right,q.left);
+    }
+    public bool IsSymmetric(TreeNode root) {
+        
+        return IsSame(root.left,root.right);
+
+        
+    }
+}
