783d6af77ee24e08fdf6aa8aa9ba6fbdeb9bdbc7
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2024-11-05 15:03:58 +0300

    Preorder added

---
+++ PreorderTraversal/main.cs
@@ -0,0 +1,31 @@
+public class TreeNode {
+
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+        this.right = right;
+     }
+ }
+ 
+public class Solution {
+
+    List<int> list = new List<int>();
+    void Preorder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        list.Add(root.val);
+        Preorder(root.left);
+        Preorder(root.right);
+    }
+    public IList<int> PreorderTraversal(TreeNode root) {
+        
+        Preorder(root);
+
+        return list;
+    }
+}
