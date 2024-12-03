1fe85ed591d4d195ab8f730257546088e614790c
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2024-12-03 10:16:32 +0300

    BrickWall

---
+++ BrickWall/main.py
@@ -0,0 +1,28 @@
+"""
+2 boyutlu dizi içerisinde verilmiş tuğlaları hayal edelim. Bu tuğlalar belirli uzunluklarla yerleştirilmiş.
+Tuğlalar arasındaki boşluklardan geçilmeye çalışılmaktadır. En az tuğlayı kıracak şekilde mevcut olan geçidi geri döndürün. 
+
+"""
+
+def leastBricks(wall):
+        """
+        :type wall: List[List[int]]
+        :rtype: int
+        """
+         
+        dicts = {0:0}
+
+        for i in wall:
+            control = 0
+            for index,j in enumerate(i):
+
+                if index != len(i)-1:
+                    control += j
+                    if control in dicts.keys():
+
+                        dicts[control] += 1 
+                    else:
+                        dicts[control] = 1
+
+        result = len(wall) - max(dicts.values())
+        return result
