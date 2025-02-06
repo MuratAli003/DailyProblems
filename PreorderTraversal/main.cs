22906ee4057a39ff330fe5666dc76f17939f3ff2
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2025-02-06 10:55:18 +0300

    PreorderTraversal

--- PreorderTraversal/main.cs
+++ PreorderTraversal/main.cs
@@ -4,7 +4,8 @@ public class TreeNode {
      public TreeNode left;
      public TreeNode right;
      
-     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null)
+    {
          this.val = val;
          this.left = left;
         this.right = right;
