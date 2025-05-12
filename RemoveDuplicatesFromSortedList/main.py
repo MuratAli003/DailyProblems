5681c2272f5f8165a26f57f8a9241aa817a64dc9
Ali Murat Yaldırak <aliimuratyaldirak@gmail.com>
2025-05-12 07:59:34 +0300

    RemoveDuplicatesFromSortedList

---
+++ RemoveDuplicatesFromSortedList/main.py
@@ -0,0 +1,21 @@
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def deleteDuplicates(self, head):
+        
+        pointer = head
+        while pointer and pointer.next:
+
+            if pointer.val == pointer.next.val:
+
+                pointer.next = pointer.next.next
+
+            else:
+                pointer = pointer.next
+        return head 
+            
+        
